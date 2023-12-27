4a833c1c3e1f5f7fbac37382df717d3026665cf9
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-27 11:37:41 +0700

    [PUSH] refac: wednesday-27th-december-2023

--- 231227/src/IntroPanel.java
+++ 231227/src/IntroPanel.java
@@ -4,9 +4,11 @@ import javax.swing.JPanel;
 
 public class IntroPanel extends JPanel {
     public IntroPanel() {
-        setBackground(Color.green);
+        setBackground(Color.RED);
         JLabel l1 = new JLabel("Tampilan Layout untuk GUI.");
+        l1.setForeground(Color.WHITE);
         JLabel l2 = new JLabel("Pilih tiap tab untuk melihat karakteristiknya ");
+        l2.setForeground(Color.WHITE);
         add(l1);
         add(l2);
     }
