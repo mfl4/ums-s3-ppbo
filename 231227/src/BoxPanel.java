215714361b047f0a784ecd42cd0d044b375f3545
mfl4 <mhdfarhanl6267@gmail.com>
2023-12-27 10:30:13 +0700

    [PUSH] feat: wednesday-27th-december-2023

---
+++ 231227/src/BoxPanel.java
@@ -0,0 +1,29 @@
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+
+public class BoxPanel extends JPanel {
+    public BoxPanel() {
+        setLayout(new BoxLayout(this,
+                BoxLayout.Y_AXIS));
+        setBackground(Color.green);
+        JButton b1 = new JButton("BUTTON 1");
+        JButton b2 = new JButton("BUTTON 2");
+        JButton b3 = new JButton("BUTTON 3");
+        JButton b4 = new JButton("BUTTON 4");
+        JButton b5 = new JButton("BUTTON 5");
+        add(b1);
+        add(Box.createRigidArea(new Dimension(0,
+                10)));
+        add(b2);
+        add(Box.createVerticalGlue());
+        add(b3);
+        add(b4);
+        add(Box.createRigidArea(new Dimension(0,
+                20)));
+        add(b5);
+    }
+}
